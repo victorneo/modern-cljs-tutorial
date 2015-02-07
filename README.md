6d9bfd1640b25e47ae581b12d0b0cbbd9dfe4873
Victor Neo <icyisamu@gmail.com>
2015-02-08 00:18:31 +0800

    Initial commit with tutorial 1

---
+++ README.md
@@ -0,0 +1,9 @@
+# modern-cljs
+
+Modern-clojurescript tutorial
+
+## License
+
+Copyright © 2015 Victor Neo
+
+Distributed under the Eclipse Public License either version 1.0  any later version.
---
+++ project.clj
@@ -0,0 +1,28 @@
+(defproject modern-cljs "0.1.0-SNAPSHOT"
+  :description "Modern-cljs tutorial"
+  :url "https://github.com/victorneo/modern-cljs"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  ;; CLJ AND CLJS source code path
+  :source-paths ["src/clj" "src/cljs"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2760"]]
+
+  ;; lein-cljsbuild plugin to build a CLJS project
+  :plugins [[lein-cljsbuild "1.0.4"]]
+
+  ;; cljsbuild options configuration
+  :cljsbuild {:builds
+              [{;; CLJS source code path
+                :source-paths ["src/cljs"]
+
+                ;; Google Closure (CLS) options configuration
+                :compiler {;; CLS generated JS script filename
+                           :output-to "resources/public/js/modern.js"
+
+                           ;; minimal JS optimization directive
+                           :optimizations :whitespace
+
+                           ;; generated JS code prettyfication
+                           :pretty-print true}}]})
